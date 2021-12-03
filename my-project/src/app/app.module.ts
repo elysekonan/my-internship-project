2e0245323f66fb60839ddc9d509d0de67a9a19e5
Elyse Konan <konanroland@gmail.com>
2021-12-03 00:31:14 -0600

    first commit

---
+++ my-project/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+  import { FlexLayoutModule } from '@angular/flex-layout';
+import { LoginComponent } from './login/login.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'login', component: HomeComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+];
+
+  @NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+  })
+  export class AppModule { }
+
+  
---
+++ my-project/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+]
